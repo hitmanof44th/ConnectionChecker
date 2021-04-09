ddd2b33cc386f7a3678c574ff07f09ce2c6a7365
jt.boron <JT.BORON@GMAIL.COM>
2021-04-09 08:11:31 -0400

    Add project files.

---
+++ ConnectionChecker/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectionChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        IPGlobalProperties properties = IPGlobalProperties.GetIPGlobalProperties();
+        TcpConnectionInformation[] connections = properties.GetActiveTcpConnections();
+
+            foreach (TcpConnectionInformation xcon in connections) 
+            {
+
+                if (xcon.RemoteEndPoint.Address.ToString() != "127.0.0.1") 
+                {
+                   
+                    if (xcon.State.ToString() == "Established")
+                    {
+                      Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                    else
+                    {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                    }
+
+                       Console.WriteLine(xcon.RemoteEndPoint.Address + ":" + xcon.RemoteEndPoint.Port + " \t\t\t" + xcon.State);
+                }
+
+            }
+
+
+            Console.WriteLine("Done Running Press Any Key To Exit");
+            Console.ReadLine();
+        }
+    }
+}
